001aecf473aaf0ad6dfc04532dfd5d982f2406a1
Kauê Santos de Souza <162394565+KaueSs7@users.noreply.github.com>
2024-10-26 18:59:01 -0300

    Update SearchScreen.test.js

--- aplicativo-loja/test/SearchScreen.test.js
+++ aplicativo-loja/test/SearchScreen.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import SearchScreen from '../screens/SearchScreen';
 
 describe('SearchScreen', () => {
   it('deve renderizar a tela de busca corretamente', () => {
-    const { getByPlaceholderText } = render(<SearchScreen />);
+    const { getByPlaceholderText } = render(
+      <NavigationContainer>
+        <SearchScreen />
+      </NavigationContainer>
+    );
 
     // Verifica se o campo de busca está presente
     expect(getByPlaceholderText('Pesquisar produtos...')).toBeTruthy();
