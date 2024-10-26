f25962d61f2785fac6cee95c54bb2053a7d10e82
Kauê Santos de Souza <162394565+KaueSs7@users.noreply.github.com>
2024-10-26 18:29:42 -0300

    Update jest.config.js

--- aplicativo-loja/jest.config.js
+++ aplicativo-loja/jest.config.js
@@ -4,13 +4,14 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-native-community|@react-native-async-storage/async-storage|@expo/vector-icons|@testing-library/react-native|expo-font)/)',
+    'node_modules/(?!(react-native|@react-native|@react-native-community|@react-native-async-storage/async-storage|@expo/vector-icons|@testing-library/react-native|expo-font|expo-modules-core)/)',
   ],
   setupFiles: [
     './node_modules/react-native-gesture-handler/jestSetup.js',
-    './jest/setup.js', 
+    './jest/setup.js',
   ],
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
   },
 };
+
