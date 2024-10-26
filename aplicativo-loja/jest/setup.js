29e493487930781157cd282b81d63bb46cf1bde8
Kauê Santos de Souza <162394565+KaueSs7@users.noreply.github.com>
2024-10-26 17:38:02 -0300

    Update setup.js

--- aplicativo-loja/jest/setup.js
+++ aplicativo-loja/jest/setup.js
@@ -2,6 +2,6 @@ import 'react-native-gesture-handler/jestSetup';
 
 jest.mock('react-native-reanimated', () => {
   const MockedReanimated = require('react-native-reanimated/mock');
-  MockedReanimated.default.call = jest.fn();
   return MockedReanimated;
 });
+
